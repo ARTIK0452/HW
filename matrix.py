9e6a9bc3ba25d2d8764581ac839c301dc4b6b836
ARTIK0452 <43519433+ARTIK0452@users.noreply.github.com>
2020-03-05 16:03:39 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,1 +1,12 @@
+size_matrix = int(input())
+number = int(input())
+number1 = [i for i in range(1,number+1)]
+print(number1[0])
+print(' ')
 
+list_db = [[number1] * size_matrix for i in range(size_matrix)]
+    
+for i in range(size_matrix):
+    for j in range(size_matrix):  
+        print(list_db[i][j], end='   ')
+    print()
